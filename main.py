918c935984a242cc9de9dd4f01e1dc50536012d2
basharm98 <167342964+basharm98@users.noreply.github.com>
2024-05-20 17:45:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
+import requests
 import telebot
-import requests,os
-from bs4 import BeautifulSoup
+from telebot.types import InlineKeyboardButton as Btn, InlineKeyboardMarkup as Mak
 
 token = "6995501333:AAG29tC7PpoFq7l6a-B1SvY3jMRILMmGaos"
 bot = telebot.TeleBot(token)
 
-    
+sent_video_messages = {}
+
 @bot.message_handler(commands=["start"])
 def start(message):
-	name = message.from_user.first_name
-	user = message.from_user.username
-	user = user.replace('@', '')
-	url = 'https://t.me/'+user
-	req = requests.get(url).text
-	soup = BeautifulSoup(req, 'html.parser')
-	photo = soup.find('meta', property="og:image")['content']
-	bio = soup.find('meta', property="og:description")['content']
+    bot.reply_to(message, 'ارسل رابط فيديو على الانستقرام')
 
-	id = message.from_user.id
+@bot.message_handler(content_types=['text'])
+def Down(message):
+    try:
+    	link = message.text
+    	json_data = {
+    	    'url': link
+    	}
+    	response = requests.post('https://insta.savetube.me/downloadPostVideo', json=json_data).json()
+    	thu = response['post_video_thumbnail']
+    	video = response['post_video_url']
+    	
+    	sent_message = bot.send_photo(message.chat.id, thu, reply_to_message_id=message.message_id, reply_markup=Mak().add(Btn('تحميل الفيديو', callback_data='vid')))
+    	
+    	sent_video_messages[sent_message.message_id] = video
+    except:
+    	bot.reply_to(message,'Invalid link')
 
-	lan = message.from_user.language_code
-	msg = bot.send_photo(message.chat.id, photo, caption=f'<b>▪ Nick: @{user}\n▪ ID: {id}\n▪ First: {name}\n▪️Bio : {bio}\n▪️ Lang: {lan}\n▪Deve : @ttxxxn</b>',parse_mode="HTML")	
-
-@bot.message_handler(func=lambda message: True)
-def all(message):
-	name = message.from_user.first_name
-	bot.reply_to(message,f'What, {name}')
-
-
+@bot.callback_query_handler(func=lambda call: call.data == 'vid')
+def all(call):
+    message_id = call.message.message_id
+    if message_id in sent_video_messages:
+        video = sent_video_messages[message_id]
+        
+        bot.delete_message(call.message.chat.id, call.message.message_id)
+        Mn = f"[تم تحميل بواسطة](ttxxxn.t.me)"
+        bot.send_video(call.message.chat.id,video,caption=Mn,parse_mode="Markdown")
+        
 bot.infinity_polling()
